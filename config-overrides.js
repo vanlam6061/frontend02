d87e8bf39b1b46bda2d2f02c32c23028b1b898a0
vanlam <vanlam6061@gmail.com>
2024-11-06 22:56:44 +0700

    02

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-};
+const { override, useBabelRc } = require('customize-cra');
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+module.exports = override(useBabelRc());
