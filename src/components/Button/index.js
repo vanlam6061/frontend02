784bdde217dcea31ba3107447c8ee86ee5ae6788
vanlam <vanlam6061@gmail.com>
2024-11-20 22:26:04 +0700

    03

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+const cx = classNames.bind(styles);
+function Button({ to, href, onClick }) {
+    let Comp = 'button';
+    return <Comp></Comp>;
+}
+
+export default Button;
